0f20aea7597ebe32f05bcaa3ae19a0e0c9a545ca
Carlos Anders <carlosanders@gmail.com>
2023-09-04 23:59:36 -0300

    [ANDERS] config inicial

---
+++ src/main/java/com/anders/carbatchapi/CarBatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.anders.carbatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarBatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarBatchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anders/carbatchapi/CarBatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anders.carbatchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
